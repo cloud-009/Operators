0900ba177ffad27d5a9cec0bfaace08cc37708e9
Rajesh Poudel <33764768+cloud-009@users.noreply.github.com>
2022-03-23 14:27:18 +0530

    Update sumofdigits.js

--- sumofdigits.js
+++ sumofdigits.js
@@ -1,8 +1,10 @@
 let value = 123;
-let sum=0;
-while (true) {
-    sum += value %10;
+let sum =0;
+while(value) {
+    sum+= value %10;
+    console.log(sum);
     value = Math.floor(value/10);
+    console.log(value);
 }
 
 console.log(sum);
